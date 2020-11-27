9c28a60d584cfdc3f75eb163b6d63fb389f02ea5
ZeenLamDev <Zeen.Lam93@gmail.com>
2020-11-27 17:40:43 +0000

    added 2 private methods to use for dock and release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,24 @@ class DockingStation
   attr_reader :bikes
 
   def release_bike
-    fail "No bikes available" if @bikes.length == 0
+    fail "No bikes available" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if @bikes.length > 20
+    fail "Docking station is full" if full?
     @bikes << @bike
     @bike = bike
   end
+
+  private
+  def full?
+    @bikes.length >= 20 ? true : false
+  end
+
+  private
+  def empty?
+    @bikes.length <= 0
+  end
+
 end
